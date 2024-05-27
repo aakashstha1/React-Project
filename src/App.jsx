e105975f7e381e077e71463e0f00319aec07a6c5
aakashstha1 <aakash.cerestha90@gmail.com>
2024-05-27 22:47:23 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-700 text-blue-500">Hello world!</h1>;
+      <h2 className="bg-slate-400">Hi, Aakash</h2>
+    </>
+  );
+}
+export default App;
